1a42ac39f4b1b22f8a32a9e7efd7d8d60b454382
Adam Feliniak <adddzik@gmail.com>
2023-12-14 00:22:09 +0100

    Kata: solve kata 8

---
+++ Codewars/8_Leap_Years.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/526c7363236867513f0005ca/train/python
+#Solution:
+
+def is_leap_year(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
