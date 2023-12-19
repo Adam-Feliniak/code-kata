6bf24b209cb844840b89aac924862804fd18be3b
Adam Feliniak <adddzik@gmail.com>
2023-12-19 22:37:39 +0100

    Kata: solve kata 16

---
+++ Codewars/16_Count_of_positives_sum_of_negatives.py
@@ -0,0 +1,5 @@
+#Count of positives / sum of negatives
+#Solution:
+
+def count_positives_sum_negatives(arr):
+    return [len([i for i in arr if i > 0]), sum([i for i in arr if i < 0])] if len(arr) != 0 else []
